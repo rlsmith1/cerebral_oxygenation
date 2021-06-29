33ec3489340ea2ce1e956319c09543f002141195
rlsmith1 <rachelsmith61097@gmail.com>
2021-06-29 12:21:46 -0400

    updated figures

--- Functions/f_filter_signal.R
+++ Functions/f_filter_signal.R
@@ -1,64 +1,21 @@
 
-## A function to find the longest segment of the signal (where t >= 200s) within 0.5 standard deviations ##
-## Purpose is to filter blips out of signal
+## A function to find the longest segment of the signal (where t >= 200s) with variance under 1000 ##
 
-## find the longest list with a standard deviation below a certain limit is to check all potential sublists
+## Since variance does not strictly increase or decrease, the best way to find the longest list with a variance
+## below a certain limit is to check all potential sublists, starting with the biggest ones and then shrinking
+## until we find a smaller one. 
 
-## our parameters: f_filter_signal(df, max_sd = 0.5)
-
-
-f_filter_signal <- function(df, max_sd) {
-  
-  start = 1
-  end = 1
-  max_start = 1
-  max_end = 0
-  
-  df <- df %>% filter(!is.na(roll_mean))
-  
-  mean <- df$roll_mean %>% mean(na.rm = TRUE)
-  sd <- df$roll_mean %>% sd(na.rm = TRUE)
-  
-  for (i in 1:nrow(df)) {
-    
-    if ((mean - max_sd*sd) < df[i,]$roll_mean && df[i,]$roll_mean < (mean + max_sd*sd)) {
-      
-      end <- i
-      
-    } else {
-      
-      start <- i
-      
-    }
-    
-    if ((end - start) > (max_end - max_start)) {
-      
-      max_end <- end
-      max_start <- start
-      
-    }
-    
-  }
-  
-  return(df[max_start:max_end,])
-  
-}
+## our parameters: f_filter_signal(df, min_length = 20000, max_var = 1000)
 
 
-
-
-
-## Alternative function where you can filter with a max variance
-
-f_filter_signal_var <- function(df, min_length, max_var){
+f_filter_signal <- function(df, min_length, max_var){
   
   for (length in nrow(df):min_length) {
     
     for (start in 1:(nrow(df) - length + 1)) {
       
       df_temp <- df[start:(start + length - 1),]
-      df_temp <- df_temp %>% mutate(start = start, length = length)
-      
+
       var <- df_temp$roll_mean %>% var(na.rm = TRUE)
       
       if (var <= max_var) {
@@ -78,7 +35,35 @@ f_filter_signal_var <- function(df, min_length, max_var){
 
 
 
+doParallel::registerDoParallel()
+f1 <- f_filter_signal(df_thc1, min_length = 20000, max_var = 1000)
 
+
+
+
+f_filter_signal <- function(df, min_length, max_var){
+  
+  for (length in nrow(df):min_length) {
+    
+    for (start in 1:(nrow(df) - length + 1)) {
+      
+      df_temp <- df[start:(start + length - 1),]
+
+      var <- df_temp$roll_mean %>% var(na.rm = TRUE)
+      
+      if (var <= max_var) {
+        
+        return(df_temp)
+        
+      }
+      
+    }
+    
+  }
+  
+  return(NULL)
+  
+}
 
 
 
